e85d1ef7cccb1b504b8bb64abeeebb3e33df016d
LucasDiaz98 <70408175+LucasDiaz98@users.noreply.github.com>
2025-06-21 16:56:06 -0300

    borrar

--- frontend/src/Component/RutaPrivada/RutaPrivada.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom'
-
-const RutaPrivada = ({ user, children }) => {
-  return user ? children : <Navigate to="/login" replace />
-}
-
-export default RutaPrivada
