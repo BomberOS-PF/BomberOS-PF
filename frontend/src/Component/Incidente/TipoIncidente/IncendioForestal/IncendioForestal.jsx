c75ad22716a6a12c156646152a570c48eee35508
Diego Ciriaci Ribone <diegociriaci@hotmail.com>
2025-10-05 19:11:43 -0300

    fix tamaño combos

--- frontend/src/Component/Incidente/TipoIncidente/IncendioForestal/IncendioForestal.jsx
+++ frontend/src/Component/Incidente/TipoIncidente/IncendioForestal/IncendioForestal.jsx
@@ -316,7 +316,7 @@ Los bomberos pueden responder "SI" o "NO" por WhatsApp para confirmar su asisten
       <div className="card shadow-sm border-0 bg-white bg-opacity-1 backdrop-blur-sm">
         <div className="card-body">
           <form>
-            <div className="mb-3">
+            <div className="mb-3 col-md-4">
               <label className="form-label text-dark d-flex align-items-center gap-2" htmlFor="caracteristicaLugar">Características del lugar *</label>
               <Select
                 options={caracteristicasLugarOptions.map(opt => ({
